3a5a5d62aed88758db033cbe4b57fc160c12e1b9
wajra <jeewanthabandara81@gmail.com>
2025-07-25 18:39:44 +0000

    Deploying to gh-pages from @ wajra/wajra.github.io@a06874f256ae88d2bc424d2afde2eedac6e38e56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,8 +31,8 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "A concise CV is shown on this page. You can download a more comprehensive CV as a PDF file.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
