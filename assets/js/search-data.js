06d6c74cc285c3ef0cca7a3200937d02e72ac667
wajra <jeewanthabandara81@gmail.com>
2025-08-05 21:47:46 +0000

    Deploying to gh-pages from @ wajra/wajra.github.io@b018a4d63c6030ac18ef3f4d50be35ace54bf514 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,20 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "Courses, Workshops, and Mentorship",
